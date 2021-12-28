d8064fe3dce002021b50349c042ec3882774a62a
Ricardo Carvalho <ricardomc.dev@gmail.com>
2021-12-28 09:44:07 -0300

    Inserindo arquivos React

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { RiErrorWarningFill } from 'react-icons/ri'
+
+export default function NotFound() {
+  return (
+    <div>
+      < RiErrorWarningFill /> Página não encontrada
+    </div>
+  )
+}
+
